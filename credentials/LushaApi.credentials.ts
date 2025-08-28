22f9c7d7aa46dc4bf55df432067003110aba62af
Carolina Portela <carolina.portela@lusha.com>
2025-08-28 09:48:25 -0400

    Add authenticate and test methods to LushaApi credentials

--- credentials/LushaApi.credentials.ts
+++ credentials/LushaApi.credentials.ts
@@ -1,23 +1,43 @@
 import {
-	ICredentialType,
-	INodeProperties,
-} from 'n8n-workflow';
-
-export class LushaApi implements ICredentialType {
-	name = 'lushaApi';
-	displayName = 'Lusha API';
-	documentationUrl = 'https://docs.lusha.com/';
-	properties: INodeProperties[] = [
-		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string',
-			typeOptions: {
-				password: true,
-			},
-			default: '',
-			required: true,
-			description: 'Your Lusha API key. You can find it in your Lusha Dashboard under API settings.',
-		},
-	];
-}
+    IAuthenticateGeneric,
+    ICredentialTestRequest,
+    ICredentialType,
+    INodeProperties,
+ } from 'n8n-workflow';
+ 
+ export class LushaApi implements ICredentialType {
+    name = 'lushaApi';
+    displayName = 'Lusha API';
+    documentationUrl = 'https://docs.lusha.com/';
+    
+    properties: INodeProperties[] = [
+        {
+            displayName: 'API Key',
+            name: 'apiKey',
+            type: 'string',
+            typeOptions: {
+                password: true,
+            },
+            default: '',
+            required: true,
+            description: 'Your Lusha API key. You can find it in your Lusha Dashboard under API settings.',
+        },
+    ];
+ 
+    authenticate: IAuthenticateGeneric = {
+        type: 'generic',
+        properties: {
+            headers: {
+                'api_key': '={{$credentials.apiKey}}',
+            },
+        },
+    };
+ 
+    test: ICredentialTestRequest = {
+        request: {
+            baseURL: 'https://api.lusha.com',
+            url: '/person',
+            method: 'GET',
+        },
+    };
+ }
